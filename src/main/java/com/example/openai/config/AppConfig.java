332efd40836ad3ded73ca33eeb38d60f15f84743
hwoo <tone8943@gmail.com>
2025-03-22 14:02:11 +0900

    ChatClient에 system message(역할) 추가. ChatClient prompt에 chatReponse 사용.

--- src/main/java/com/example/openai/config/AppConfig.java
+++ src/main/java/com/example/openai/config/AppConfig.java
@@ -8,8 +8,15 @@ import org.springframework.context.annotation.Configuration;
 public class AppConfig {
 
     // ChatClient <---- OPEN_API_KEY ----> LLM(openai)
+//    @Bean
+//    public ChatClient chatClient(ChatClient.Builder chatClientBuilder) {
+//        return chatClientBuilder.build();
+//    }
     @Bean
     public ChatClient chatClient(ChatClient.Builder chatClientBuilder) {
-        return chatClientBuilder.build();
+        // System Message: LLM에 역할을 부여
+        return chatClientBuilder
+                .defaultSystem("당신은 교육 튜터입니다. 개념을 명확하고 간단하게 설명하세요.")
+                .build();
     }
 }
--- src/main/java/com/example/openai/service/ChatService.java
+++ src/main/java/com/example/openai/service/ChatService.java
@@ -16,6 +16,16 @@ public class ChatService {
         return chatClient.prompt() // 프롬프트 생성
                 .user(message) // 사용자 메시지
                 .call() // ai model 호출
-                .content(); // 요청 정보를 받는 부분
+                .content(); // ChatResponse에서 문자열로 요청 정보를 받는 부분
+    }
+
+    public String chatmessage(String message) {
+        return chatClient.prompt() // 프롬프트 생성
+                .user(message) // 사용자 메시지
+                .call() // ai model 호출
+                .chatResponse() // ChatResponse
+                .getResult()
+                .getOutput()
+                .getText(); // getContent -> getText로 변경됨
     }
 }

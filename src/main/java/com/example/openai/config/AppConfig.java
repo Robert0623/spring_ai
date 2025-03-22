c80d67bf763dfbefe0a0d6e0c144189ed90a9e7a
hwoo <tone8943@gmail.com>
2025-03-22 16:35:59 +0900

    message advisor로 이전 채팅을 LLM이 기억하도록

--- src/main/java/com/example/openai/config/AppConfig.java
+++ src/main/java/com/example/openai/config/AppConfig.java
@@ -1,6 +1,8 @@
 package com.example.openai.config;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.InMemoryChatMemory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,9 +22,9 @@ public class AppConfig {
     @Bean
     public ChatClient chatClient(ChatClient.Builder chatClientBuilder) {
         // System Message: LLM에 역할을 부여
-        return chatClientBuilder
-                // .defaultSystem("당신은 교육 튜터입니다. 개념을 명확하고 간단하게 설명하세요.")
-                // .defaultSystem(resource)
-                .build();
+        // return chatClientBuilder.defaultSystem("당신은 교육 튜터입니다. 개념을 명확하고 간단하게 설명하세요.").build();
+        // return chatClientBuilder.defaultSystem(resource).build();
+        // return chatClientBuilder.build();
+        return chatClientBuilder.defaultAdvisors(new MessageChatMemoryAdvisor(new InMemoryChatMemory())).build();
     }
 }
